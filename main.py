d2910cc5077b3d00cddb5c8c4a84d75a6413b158
Xiran(April)_Sun <xs298@cornell.edu>
2019-09-23 23:15:48 -0400

    link login and register pages

--- main.py
+++ main.py
@@ -5,17 +5,30 @@ app = Flask(__name__)
 @app.route("/login",methods=['POST','GET'])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin123':
-            error= "sorry"
-        else:
-            return redirect(url_for('index'))
+    # if request.method == 'POST':
+    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin123':
+    #         error= "sorry"
+    #     else:
+    #         return redirect(url_for('index'))
     return render_template('login.html',error=error)
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def index():
     title = 'TwitterDashboardHomePage'
     return render_template('index.html', title=title)
+    # if request.method == 'POST':
+    #     if request
+    # return redirect(url_for('login'))
+
+@app.route("/register",methods=['POST','GET'])
+def register():
+    error = None
+    # if request.method == 'POST':
+    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin123':
+    #         error= "sorry"
+    #     else:
+    #         return redirect(url_for('index'))
+    return render_template('register.html',error=error)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=8080, debug=True)
+    app.run(host='0.0.0.0',port=8080, debug=True)
