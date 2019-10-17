fe4417fee375dbf139e3a8f0c4c7187a7fdcce7b
Kelly SUN <js3422@cornell.edu>
2019-10-17 16:41:12 -0400

    add sampling *2

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from flask import render_template, Flask, redirect, request, session
-from google.cloud import datastore
-from util.functions import alreadyExist, store_user_profile, random_salt, hash_pbkdf2, store_tweets
+# from google.cloud import datastore
+from util.functions import random_salt, hash_pbkdf2
+# , alreadyExist, store_user_profile, store_tweets
 import tweepy, logging
 from util.StreamListener import StreamListener
-# from TwitterAPI import TwitterAPI
-# from http import HTTPStatus
-# import util.Twitter, hashlib, hmac, base64, os, logging, json
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
 import pandas as pd
 
-datastore_client = datastore.Client('twitterdashboard')
+# datastore_client = datastore.Client('twitterdashboard')
 
 app = Flask(__name__)
 app.secret_key = 'tsdhisiusdfdsfaSecsdfsdfrfghdetkey'
@@ -20,29 +18,19 @@ access_token = '364156861-cSyt6v8Rjg4n8aVxRqI7stklhtvv69raNR7X3Tp9'
 access_token_secret = 'q9AppxYixPtI7HAi4Fxxd2i6Nl6ESGDqzCVqVOOFjr0FB'
 consumer_key = '0IvIaXCm8CUHeuayBiFS3Blwd' 
 consumer_secret = 'WlgHUfC7waVlRrktuyySBRQHwVSBPFpxEud2hGY08i83NFXpNk'
-envname = 'kellyhook'
-webhook_url = ''
 perspective_api_key = 'AIzaSyAQzy172qDSsB89r-8sKcRKoLKncsHq8eU'
 
-callback_uri = 'https://twitterdashboard.appspot.com/callback'
+callback_uri = 'https://aa3a6ae5.ngrok.io/callback'
 request_token_url = 'https://api.twitter.com/oauth/request_token'
 authorization_url = 'https://api.twitter.com/oauth/authorize'
 access_token_url = 'https://api.twitter.com/oauth/access_token'
+
+service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=perspective_api_key)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     title = 'TwitterDashboardHomePage'
     return render_template('index.html')
-#     tweet_replies = [
-#         {'tid': 1181568448004050951, 
-#         'context': 'As President, I leaned on @AmbassadorRice’s experience, expertise, and willingness to tell me what I needed to hear… https://t.co/oWx2obfDF5', 'hashtag': [], 
-#         'reply': {'uid': 1174477973879238657, 'uname': 'mpy', 'reply': '@BarackObama \nObama still control mainstream media and many federal government agencies. the media is party of the… https://t.co/dZsNVllSfW'}}, 
-#         {'tid': 1181568448004050951, 'context': 'As President, I leaned on @AmbassadorRice’s experience, expertise, and willingness to tell me what I needed to hear… https://t.co/oWx2obfDF5', 'hashtag': [], 
-#         'reply': {'uid': 51639017, 'uname': 'Valerie Cartwright',
-#          'reply': 'RT @ReasePaino: @BarackObama @AmbassadorRice https://t.co/NjZFQ2HD0Y'}}
-#         ]
-#     return render_template('dash.html', len = len(tweet_replies), result = tweet_replies)
-
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -53,19 +41,19 @@ def login():
         session['password'] = request.form.get("password")
         if len(session['username']) != 0:
             loaded = True
-            key = datastore_client.key("user_file", session['username'])
-            entity = datastore_client.get(key)
-            print(entity)
-            if not entity:
-                print('Invalid username')
-                error = "No username found"
-                loaded = False
-            elif entity["saltedPw"] != hash_pbkdf2(session['password'], entity['salt']):
-                print('Invalid password')
-                error = "Please use make sure your password is correct!"
-                loaded = False
+            # key = datastore_client.key("user_file", session['username'])
+            # entity = datastore_client.get(key)
+            # print(entity)
+            # if not entity:
+            #     print('Invalid username')
+            #     error = "No username found"
+            #     loaded = False
+            # elif entity["saltedPw"] != hash_pbkdf2(session['password'], entity['salt']):
+            #     print('Invalid password')
+            #     error = "Please use make sure your password is correct!"
+            #     loaded = False
     if loaded:
-      return redirect('/dash')
+        return redirect('/dash')
     else:
         return render_template('login.html', error=error)
 
@@ -83,10 +71,9 @@ def register():
             if session['password'] != rePassword:
                 error = "Make sure the passwords match with each other."
                 loaded = False
-            if alreadyExist(datastore_client, session['username']):
-                error = "Ooops! The username has already exist, please use another!"
-                loaded = False
-            # store_user_profile(datastore_client, session['username'], session['password'])
+            # if alreadyExist(datastore_client, session['username']):
+            #     error = "Ooops! The username has already exist, please use another!"
+            #     loaded = False
     if loaded:
         return redirect('/auth')
     else:
@@ -119,12 +106,17 @@ def callback():
     session['token'] = (auth.access_token, auth.access_token_secret)
     logging.info(auth.access_token, auth.access_token_secret)
     # save access_token, access_token_secret to datastore for reuse
-    store_user_profile(datastore_client, session['username'], session['password'], auth.access_token, auth.access_token_secret)
+    # store_user_profile(datastore_client, session['username'], session['password'], auth.access_token, auth.access_token_secret)
 
     return redirect('/app')
 
 
 @app.route('/app') # rate limit, might use stream api
+
+# TODO 10.17:
+# need to get previous mute list somewhere
+# reform the code to include get_tweets, get_muted, etc.
+
 def get_tweets(): # old version in StreamListener
     token, token_secret = session['token']
 
@@ -141,7 +133,6 @@ def get_tweets(): # old version in StreamListener
 
     # scrape initial set of tweets
     # only select tweets that have replies (would be hard for testing)
-    service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=perspective_api_key)
     tweets = api.user_timeline(screen_name=session['username'], count=10) # max count = 200
     tweet_replies = []
     for tweet in tweets:
@@ -150,7 +141,7 @@ def get_tweets(): # old version in StreamListener
         tmp['context'] = tweet.text
         tmp['hashtag'] = tweet.entities['hashtags']
 
-        for reply in tweepy.Cursor(api.search, q=session['username'], since_id=tweet.id_str, result_type="mixed").items(2):
+        for reply in tweepy.Cursor(api.search, q=session['username'], since_id=tweet.id_str, result_type="mixed").items(5):
             if reply.in_reply_to_status_id_str == tweet.id_str:
                 # comment = {'uid': reply.user.id_str, 'uname': reply.user.screen_name, 'reply': reply.text}
                 tmp['reply_user_id'] = reply.user.id_str
@@ -161,34 +152,27 @@ def get_tweets(): # old version in StreamListener
                 # tweet_replies used for display in dash.html
                 tweet_replies.append(tmp.copy())
                 # store to database & for training
-                # store_tweets(datastore_client, tweet.id_str, 
+                # store_tweets(datastore_client, '_'.join(tweet.id_str, reply.id_str), 
                 #             reply_to_id=tweet.user.id_str,
                 #             reply_to_name=session['username'], 
                 #             context=tweet.text, 
                 #             context_hashtags=tweet.entities['hashtags'], 
-                #             reply_id=reply.id_str,
                 #             reply_user_id=reply.user.id_str, 
                 #             reply_user_name=reply.user.screen_name, 
-                #             text=reply.text)
-                store_tweets(datastore_client, '_'.join(tweet.id_str, reply.id_str), 
-                            reply_to_id=tweet.user.id_str,
-                            reply_to_name=session['username'], 
-                            context=tweet.text, 
-                            context_hashtags=tweet.entities['hashtags'], 
-                            reply_user_id=reply.user.id_str, 
-                            reply_user_name=reply.user.screen_name, 
-                            text=reply.text,
-                            reply_hashtags=reply.entities['hashtags'],
-                            toxicity=tmp['toxicity'],
-                            identity_attack=tmp['identity_attack'],
-                            insult=tmp['insult'],
-                            profanity=tmp['profanity'],
-                            threat=tmp['threat'],
-                            sexually_explicit=tmp['sexually_explicit'],
-                            flirtation=tmp['flirtation'])
-    
-    tweet_replies = get_samples(tweet_replies)
-    tweet_replies = [_, row for row in tweet_replies.iterrows()]
+                #             text=reply.text,
+                #             reply_hashtags=reply.entities['hashtags'],
+                #             toxicity=tmp['toxicity'],
+                #             identity_attack=tmp['identity_attack'],
+                #             insult=tmp['insult'],
+                #             profanity=tmp['profanity'],
+                #             threat=tmp['threat'],
+                #             sexually_explicit=tmp['sexually_explicit'],
+                #             flirtation=tmp['flirtation'])
+    # option 1: sample by perspective scores
+    # tweet_replies = get_samples_1(tweet_replies)
+
+    # option 2: group by reply users, threshold at 5
+    tweet_replies = get_samples_2(tweet_replies)
     print(tweet_replies)
 
     # display for label
@@ -196,7 +180,6 @@ def get_tweets(): # old version in StreamListener
     # after labeling
     return render_template('dash.html', len = len(tweet_replies), result = tweet_replies)
   
-
 def get_perspective(tmp):
     analyze_request = {'comment': {'text': tmp['text']},
                         'requestedAttributes': {'TOXICITY': {}, 
@@ -206,29 +189,33 @@ def get_perspective(tmp):
                         'THREAT':{}, 
                         'SEXUALLY_EXPLICIT':{}, 
                         'FLIRTATION':{}}}
-    try:
-        response = service.comments().analyze(body=analyze_request).execute()
+    one_more_time = True # flag if need to sleep before getting the scores
+    while one_more_time:
+        one_more_time = False
+        try:
+            response = service.comments().analyze(body=analyze_request).execute()
 
-        tmp['toxicity'] = response['attributeScores']['TOXICITY']['summaryScore']['value']
-        tmp['identity_attack'] = response['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value']
-        tmp['insult'] = response['attributeScores']['INSULT']['summaryScore']['value']
-        tmp['profanity'] = response['attributeScores']['PROFANITY']['summaryScore']['value']
-        tmp['threat'] = response['attributeScores']['THREAT']['summaryScore']['value']
-        tmp['sexually_explicit'] = response['attributeScores']['SEXUALLY_EXPLICIT']['summaryScore']['value']
-        tmp['flirtation'] = response['attributeScores']['FLIRTATION']['summaryScore']['value']
-        
-    except HttpError as e:
-        print('\n----------------------------------------------------')
-        # all scores should be nan
-        if e.resp.status == 429:
-            print('Sleeping...')
-            time.sleep(1)
-        if e.resp.status == 400:
-            print(e)
-            print(tmp['text'])    
+            tmp['toxicity'] = response['attributeScores']['TOXICITY']['summaryScore']['value']
+            tmp['identity_attack'] = response['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value']
+            tmp['insult'] = response['attributeScores']['INSULT']['summaryScore']['value']
+            tmp['profanity'] = response['attributeScores']['PROFANITY']['summaryScore']['value']
+            tmp['threat'] = response['attributeScores']['THREAT']['summaryScore']['value']
+            tmp['sexually_explicit'] = response['attributeScores']['SEXUALLY_EXPLICIT']['summaryScore']['value']
+            tmp['flirtation'] = response['attributeScores']['FLIRTATION']['summaryScore']['value']
+            
+        except HttpError as e:
+            print('\n----------------------------------------------------')
+            # all scores should be nan
+            if e.resp.status == 429:
+                one_more_time = True
+                print('Sleeping...')
+                time.sleep(1)
+            if e.resp.status == 400:
+                print(e)
+                print(tmp['text'])    
     return tmp
 
-def get_samples(tweet_replies):
+def get_samples_1(tweet_replies):
     tweet_replies = pd.DataFrame.from_records(tweet_replies)
     samples = set()
     for type in ['toxicity', 'identity_attack', 'insult', 'profanity', 'threat', 'sexually_explicit', 'flirtation']:
@@ -236,8 +223,14 @@ def get_samples(tweet_replies):
         for idx in list(indices):
             samples.add(idx)
     tweet_replies = tweet_replies.iloc[list(samples)]
+    tweet_replies = tweet_replies.to_dict('records')
     return tweet_replies
 
+def get_samples_2(tweet_replies):
+    tweet_replies = pd.DataFrame.from_records(tweet_replies)
+    tweet_replies = tweet_replies.groupby('reply_user_id').apply(lambda x: x.sample(n=2))    
+    tweet_replies = tweet_replies.to_dict('records')
+    return tweet_replies
 
 @app.route("/dash")
 def dash():
--- util/functions.py
+++ util/functions.py
@@ -1,4 +1,4 @@
-from google.cloud import datastore
+# from google.cloud import datastore
 from hashlib import pbkdf2_hmac
 from random import getrandbits
 
@@ -11,20 +11,20 @@ def alreadyExist(client, username):
         return True
 
       
-def store_user_profile(client, username, password, access_token, access_token_secret):
-    kind = 'user_file'
-    name = username
-    task_key = client.key(kind, name)
-    entity = datastore.Entity(key=task_key)
-    salt = random_salt()
-    saltedPw = hash_pbkdf2(password, salt)
-    entity['username'] = username
-    entity['saltedPw'] = saltedPw
-    entity['salt'] = salt
-    entity['access_token'] = access_token
-    entity['access_token_secret'] = access_token_secret
-    client.put(entity)
-    print('Saved {}: {}'.format(entity.key.name, entity['saltedPw']))
+# def store_user_profile(client, username, password, access_token, access_token_secret):
+#     kind = 'user_file'
+#     name = username
+#     task_key = client.key(kind, name)
+#     entity = datastore.Entity(key=task_key)
+#     salt = random_salt()
+#     saltedPw = hash_pbkdf2(password, salt)
+#     entity['username'] = username
+#     entity['saltedPw'] = saltedPw
+#     entity['salt'] = salt
+#     entity['access_token'] = access_token
+#     entity['access_token_secret'] = access_token_secret
+#     client.put(entity)
+#     print('Saved {}: {}'.format(entity.key.name, entity['saltedPw']))
 
 def random_salt():
     return getrandbits(128).to_bytes(16, byteorder='little').hex()
@@ -32,18 +32,18 @@ def random_salt():
 def hash_pbkdf2(x, salt):
     return pbkdf2_hmac('sha256', x.encode('utf-8'), bytes.fromhex(salt), 100000).hex()
 
-def store_tweets(client, context_id, reply_to_id, reply_to_name, context, context_hashtags, reply_id, reply_user_id, reply_user_name, text):
-    kind = 'tweets'
-    name = context_id
-    task_key = client.key(kind, name)
-    entity = datastore.Entity(key=task_key)
-    entity['reply_to_id'] = reply_to_id
-    entity['reply_to_name'] = reply_to_name
-    entity['context'] = context
-    entity['context_hashtags'] = context_hashtags
-    entity['reply_id'] = reply_id
-    entity['reply_user_id'] = reply_user_id
-    entity['reply_user_name'] = reply_user_name
-    entity['text'] = text
-    client.put(entity)
-    print('Saved', entity.key.name, entity)
+# def store_tweets(client, context_id, reply_to_id, reply_to_name, context, context_hashtags, reply_id, reply_user_id, reply_user_name, text):
+#     kind = 'tweets'
+#     name = context_id
+#     task_key = client.key(kind, name)
+#     entity = datastore.Entity(key=task_key)
+#     entity['reply_to_id'] = reply_to_id
+#     entity['reply_to_name'] = reply_to_name
+#     entity['context'] = context
+#     entity['context_hashtags'] = context_hashtags
+#     entity['reply_id'] = reply_id
+#     entity['reply_user_id'] = reply_user_id
+#     entity['reply_user_name'] = reply_user_name
+#     entity['text'] = text
+#     client.put(entity)
+#     print('Saved', entity.key.name, entity)
